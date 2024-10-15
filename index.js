8b622de3c685e80d8ad39baf92a4df5c7c3e82a6
Ksenia Grebenshchikova <ksena@Ksenias-Air.cg.shawcable.net>
2024-10-15 12:16:32 -0600

    added PUT

--- index.js
+++ index.js
@@ -21,38 +21,29 @@ app.post('/api/items', (req,res) => {
     res.json(items)
 });
 
-app.delete('/api/items/:id', (req, res)=> {
-    let { id } = req.params;
-    let idToBeDeleted = items.find(item => item.id === id);
-
-    if(idToBeDeleted){
-        res.json({
-            message: "Item removed",
-            items: items.filter(item => item.id !== id)
-        })
-    } else {
-        res.status(404).json({message: `item is not found`})
-    }
-});
-
 app.put('/api/items/:name', (req, res)=> {
     let { name } = req.params;
     let itemToBeUpdated = items.find(item => item.name === name);
 
     if(itemToBeUpdated){
-        const newName = req.body;
+        const newName = {
+            name: req.body.name,
+            id: req.body.id,
+            price: req.body.price
+        }
 
         items.forEach(item => {
             if(item.name === req.params.name){
                 item.name = newName ? newName.name : item.name;
+                item.id = newName ? newName.id : item.id;
+                item.price = newName ? newName.price : item.price;
                 res.json({message: `Item description has been updated`, item})
-            } else {
-                res.status(404).json({message: `Item ${req.params.name} can't be found`})
-            }
+            } 
         })
+    } else {
+        res.status(404).json({message: `Item ${req.params.name} can't be found`})
     }
 })
-
 
 app.listen(4000, ()=> {
     console.log(`Hi it's working on port 4000`)
--- items.js
+++ items.js
@@ -15,20 +15,15 @@ const items = [
         price: 20
     },
     {
-        name: "coat",
-        id: "5ee",
-        price: 20
-    },
-    {
         name: "dress",
         id: "4dd",
         price: 20
+    },
+    {
+        name: "coat",
+        id: "5ee",
+        price: 20
     }
-    // {
-    //     name: "coat",
-    //     id: "5ee",
-    //     price: 20
-    // }
 ];
 
 module.exports = items;
